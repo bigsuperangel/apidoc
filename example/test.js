bfddeaac85fb8e1c872fd5e2b5dee0beda03dba3
bigsuperangel <bigsuperangel@gmail.com>
2016-06-13 16:09:50 +0800

    apidoc

---
+++ example/test.js
@@ -0,0 +1,12 @@
+/**
+ * @api {post} /test/:id Request User information
+ * @apiName GetTest
+ * @apiGroup Test
+ *
+ * @apiParam {Number} id Users unique ID.
+ *
+ * @apiSuccess {String} name Firstname of the User.
+ * @apiSuccess {String} title  Lastname of the User.
+ */
+function getTest(){return ;}
+
---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
+
+// 监视文件改动并重新载入
+gulp.task('serve', function() {
+  browserSync({
+    server: {
+      baseDir: 'doc'
+    }
+  });
+
+  gulp.watch(['*.html', 'styles/**/*.css', 'scripts/**/*.js'], {cwd: 'doc'}, reload);
+});
+
+gulp.task('default',['serve']);
